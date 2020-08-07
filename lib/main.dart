f8bfa31377ed6970edb6c3f3952b4b90ff77e851
Movin Silva <movinpinsara@gmail.com>
2020-08-07 15:01:37 +0530

    dev preview added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:ecode/authPage/authScreen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(),
+  ),
+);
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
