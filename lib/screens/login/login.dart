a870fa20d5e146dc22024fb98917d83193b56a0f
Movin Silva <movinpinsara@gmail.com>
2020-08-10 23:29:52 +0530

    register layout created

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -5,7 +5,6 @@ import 'package:google_fonts/google_fonts.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -18,40 +17,48 @@ class LoginPage extends StatelessWidget {
                 height: size.height * 0.3,
                 width: size.width,
               ),
-              Widgets.buildForm(context),
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: 36.0, right: 36.0, top: 30,
-                        bottom: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [Widgets.buildSignInBtn()],
+              Container(
+                height: size.height * 0.495,
+                width: size.width,
+                child: PageView(
+                  children: <Widget>[
+                    Widgets.loginForm(context),
+                    Widgets.registerForm(context),
+                  ],
                 ),
               ),
+
               Widgets.buildContactUs(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Widgets.buildSocialIcons(context, "assets/images/facebooklo"
+                  Widgets.buildSocialIcons(
+                      context,
+                      "assets/images/facebooklo"
                       "go.png",
                       45),
-                  Widgets.buildSocialIcons(context, "assets/images/instalogo"
+                  Widgets.buildSocialIcons(
+                      context,
+                      "assets/images/instalogo"
                       ".jpg",
                       35),
-                  SizedBox(width: 8,),
-                  Widgets.buildSocialIcons(context, "assets/images/linkedinlogo"
-                      ".webp",35)
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Widgets.buildSocialIcons(
+                      context,
+                      "assets/images/linkedinlogo"
+                      ".webp",
+                      35)
                 ],
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal:
-                25),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
                 child: Text(
                   "Powered by Ecode Software Solutions (Pvt) Ltd",
                   style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 12
-                  ),
+                      fontWeight: FontWeight.w300, fontSize: 12),
                   textAlign: TextAlign.center,
                 ),
               )
--- lib/screens/login/widgets/widgets.dart
+++ lib/screens/login/widgets/widgets.dart
@@ -4,124 +4,261 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Widgets {
-
   static TextEditingController studentIdContoller = TextEditingController();
   static TextEditingController studentPasswordController =
-  TextEditingController();
+      TextEditingController();
+  static TextEditingController studentNICController = TextEditingController();
+  static TextEditingController studentConfirmPasswordController =
+      TextEditingController();
 
-  static Padding buildForm(
-      BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20),
-      child: Container(
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              Text("Login",
-                  style: GoogleFonts.poppins(
-                    fontSize: 40,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal,
-                    letterSpacing: 1.2,
-                  )),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: studentIdContoller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Student Id",
-                  labelStyle: GoogleFonts.poppins(fontWeight: FontWeight.w600),
-                ),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                controller: studentPasswordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                  labelStyle: GoogleFonts.poppins(fontWeight: FontWeight.w600),
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+  static Column loginForm(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  InkWell(
-                    child: Text(
-                      "forgot password ?",
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text("Login",
                       style: GoogleFonts.poppins(
-                          color: Colors.blueAccent,
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.w500),
+                        fontSize: 40,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        letterSpacing: 1.2,
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: studentIdContoller,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Student Id",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
                     ),
-                    onTap: () {
-                      showCupertinoDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return CupertinoAlertDialog(
-                              content: Text("Forgot Passwort ?"),
-                              actions: [
-                                FlatButton(
-                                  child: Text(
-                                    "Close",
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.blueAccent),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                                FlatButton(
-                                  child: Text(
-                                    "Yes",
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.blueAccent),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                )
-                              ],
-                            );
-                          });
-                    },
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    controller: studentPasswordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        child: Text(
+                          "forgot password ?",
+                          style: GoogleFonts.poppins(
+                              color: Colors.blueAccent,
+                              decoration: TextDecoration.underline,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        onTap: () {
+                          showCupertinoDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return CupertinoAlertDialog(
+                                  content: Text("Forgot Passwort ?"),
+                                  actions: [
+                                    FlatButton(
+                                      child: Text(
+                                        "Close",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.blueAccent),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    FlatButton(
+                                      child: Text(
+                                        "Yes",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.blueAccent),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    )
+                                  ],
+                                );
+                              });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
                   ),
                 ],
               ),
-              SizedBox(
-                height: 20,
-              )
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0.0, 15.0),
+                      color: Colors.black12,
+                      blurRadius: 15),
+                  BoxShadow(
+                      offset: Offset(0.0, -10.0),
+                      color: Colors.black12,
+                      blurRadius: 10)
+                ]),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+              left: 36.0, right: 36.0, top: 30, bottom: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text('Slide to Register  '),
+                  Icon(Icons.arrow_forward_ios),
+                  Icon(Icons.arrow_forward_ios),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Widgets.buildSignInBtn(),
+                ],
+              ),
             ],
           ),
         ),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            boxShadow: [
-              BoxShadow(
-                  offset: Offset(0.0, 15.0),
-                  color: Colors.black12,
-                  blurRadius: 15),
-              BoxShadow(
-                  offset: Offset(0.0, -10.0),
-                  color: Colors.black12,
-                  blurRadius: 10)
-            ]),
-      ),
+      ],
+    );
+  }
+
+  static Column registerForm(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text("Register",
+                      style: GoogleFonts.poppins(
+                        fontSize: 40,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        letterSpacing: 1.2,
+                      )),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  TextFormField(
+                    controller: studentIdContoller,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Student Id",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: studentNICController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "NIC no",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: studentPasswordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: studentConfirmPasswordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Confirm Password",
+                      labelStyle:
+                          GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0.0, 15.0),
+                      color: Colors.black12,
+                      blurRadius: 15),
+                  BoxShadow(
+                      offset: Offset(0.0, -10.0),
+                      color: Colors.black12,
+                      blurRadius: 10)
+                ]),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+              left: 36.0, right: 36.0, top: 8, bottom: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [Widgets.buildSignUpBtn()],
+          ),
+        ),
+      ],
     );
   }
 
@@ -154,6 +291,37 @@ class Widgets {
                       colors: [Color(0xFF17ead9), Color(0xFF6078ea)]),
                   borderRadius: BorderRadius.circular(8.0)),
             ));
+  }
+
+  static Builder buildSignUpBtn() {
+    return Builder(
+        builder: (context) => Container(
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                authentication(context);
+              },
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Text("Sign Up",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontSize: 25)),
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xFF6078ea).withOpacity(.9),
+                    offset: Offset(0.0, 8.0),
+                    blurRadius: 8.0)
+              ],
+              gradient: LinearGradient(
+                  colors: [Color(0xFF17ead9), Color(0xFF6078ea)]),
+              borderRadius: BorderRadius.circular(8.0)),
+        ));
   }
 
   static Padding buildContactUs() {
